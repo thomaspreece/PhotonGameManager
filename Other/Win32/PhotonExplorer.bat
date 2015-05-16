d2293cb99d6912741346104fa253de130e58170e
Thomas Preece <github@thomaspreece.com>
2015-05-16 14:32:43 +0100

    Added some hints to thegamesdb.net Lua for sorting algorithm. Finished First start wizard. Added DebugLog option to Settings Window. Converted Photon to FreeImage Library.

--- Other/Win32/PhotonExplorer.bat
+++ Other/Win32/PhotonExplorer.bat
@@ -1,7 +1,7 @@
 @echo off
 REM the line above turns off all the commands being printed to the commandline
 REM PhotonExplorer has a problem being executed from PhotonRunner directly so this bat file is used as an intermediate instead
-REM If you are worried about this file, either contact PhotonGameManager team for more info, or remove all the lines below (This will also stop Cabinate mode for PhotonExplorer)
+REM If you are worried about this file, either contact PhotonGameManager team for more info, or remove all the lines below (This will also stop Cabinet mode for PhotonExplorer)
 REM the line below tells users that PhotonExplorer.exe is being executed
 echo Running PhotonExplorer.exe
 REM the line below is the easiest way to delay the execution of script by 3000 ms = 3 secs
