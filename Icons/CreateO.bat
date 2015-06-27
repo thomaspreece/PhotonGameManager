b6ac9db6a4e06c1b77a40ec5dab4ef3b8c74c598
Thomas Preece <thomaspreece10@gmail.com>
2015-06-27 18:20:27 +0100

    Updated Icons/Resource File Generation Code

--- Icons/CreateO.bat
+++ Icons/CreateO.bat
@@ -1,6 +1,7 @@
-cd C:\MinGW\bin
-windres -i C:\GameManagerV4\Icons\PhotonDownloader.rc -o C:\GameManagerV4\Icons\PhotonDownloader.o
-windres -i C:\GameManagerV4\Icons\PhotonExplorer.rc -o C:\GameManagerV4\Icons\PhotonExplorer.o
-windres -i C:\GameManagerV4\Icons\PhotonFrontEnd.rc -o C:\GameManagerV4\Icons\PhotonFrontEnd.o
-windres -i C:\GameManagerV4\Icons\PhotonManager.rc -o C:\GameManagerV4\Icons\PhotonManager.o
-windres -i C:\GameManagerV4\Icons\PhotonUpdater.rc -o C:\GameManagerV4\Icons\PhotonUpdater.o
+cd C:\MinGW32\bin
+windres -i Z:/gameManager/GameManagerV4/Icons/PhotonDownloader.rc -o Z:/gameManager/GameManagerV4/Icons/PhotonDownloader.o
+windres -i Z:/gameManager/GameManagerV4/Icons/PhotonExplorer.rc -o Z:/gameManager/GameManagerV4/Icons/PhotonExplorer.o
+windres -i Z:/gameManager/GameManagerV4/Icons/PhotonFrontEnd.rc -o Z:/gameManager/GameManagerV4/Icons/PhotonFrontEnd.o
+windres -i Z:/gameManager/GameManagerV4/Icons/PhotonManager.rc -o Z:/gameManager/GameManagerV4/Icons/PhotonManager.o
+windres -i Z:/gameManager/GameManagerV4/Icons/PhotonUpdater.rc -o Z:/gameManager/GameManagerV4/Icons/PhotonUpdater.o
+windres -i Z:/gameManager/GameManagerV4/Icons/PhotonRunner.rc -o Z:/gameManager/GameManagerV4/Icons/PhotonRunner.o
