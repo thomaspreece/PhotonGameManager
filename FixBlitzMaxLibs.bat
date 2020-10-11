564c5f530e040365361b7cd1eace357b460cd096
Thomas Preece <thomaspreece10@gmail.com>
2020-10-11 19:42:07 +0100

    feat: started work on instructions

---
+++ FixBlitzMaxLibs.bat
@@ -0,0 +1,12 @@
+@echo off
+@set dirMinGW=C:\MinGW
+@set minGWVersionString=4.7.1
+@set dirBlitzmax=C:\Blitzmax
+
+copy %dirMinGW%\bin\ar.exe %dirBlitzmax%\bin /Y
+copy %dirMinGW%\bin\ld.exe %dirBlitzmax%\bin /Y
+
+for /f "delims=" %%i in ('dir %dirBlitzmax%\lib /b') do (
+copy "%dirMinGW%\lib\%%i" "%dirBlitzmax%\lib\%%i" /z /y
+copy "%dirMinGW%\lib\gcc\mingw32\%minGWVersionString%\%%i" "%dirBlitzmax%\lib\%%i" /z /y
+)
